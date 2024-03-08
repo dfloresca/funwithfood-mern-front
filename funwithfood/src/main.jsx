6cd6eb8a1733b6b23b35b9733c0fdd2439519c5c
Daniel Floresca <dfloresca@gmail.com>
2024-03-08 07:49:20 -0800

    feat: added browser router and setup some routes

--- funwithfood/src/main.jsx
+++ funwithfood/src/main.jsx
@@ -7,3 +7,43 @@ import {
 import App from "./App";
 import Recipe from "./components/Recipe";
 import RecipeList from "./components/RecipeList";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <RecipeList />
+      },
+    ],
+  },
+  {
+    path: "/edit/:id",
+    element: <App />,
+    children: [
+      {
+        path: "/edit/:id",
+        element: <Recipe />,
+      },
+    ],
+  },
+  {
+    path: "/create",
+    element: <App />,
+    children: [
+      {
+        path: "/create",
+        element: <Recipe />,
+      },
+    ],
+  },
+]);
+
+ReactDom.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
