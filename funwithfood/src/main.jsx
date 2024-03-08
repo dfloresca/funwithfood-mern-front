edf6c92e67346ae72d5e91136f83d54860c65d1b
Daniel Floresca <dfloresca@gmail.com>
2024-03-08 07:29:10 -0800

    feat: began building react router

--- funwithfood/src/main.jsx
+++ funwithfood/src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import * as React from "react";
+import * as ReactDom from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+import Recipe from "./components/Recipe";
+import RecipeList from "./components/RecipeList";
